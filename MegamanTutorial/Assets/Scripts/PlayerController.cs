bc00de732a77e17425a49b2d0fbf7e898e39afde
masayume <masayume.blog@gmail.com>
2021-05-16 17:18:30 +0200

    simple player input

--- MegamanTutorial/Assets/Scripts/PlayerController.cs
+++ MegamanTutorial/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Animator animator; // to control animations
-    BoxCollider2D box2d; // to control jump
+    Animator animator;
+    BoxCollider2D box2d;
     Rigidbody2D rb2d;
-    SpriteRenderer sprite; // explosion from the center of the sprite
+    SpriteRenderer sprite;
 
     float keyHorizontal;
+    float keyVertical;
     bool keyJump;
     bool keyShoot;
+
     bool isGrounded;
     bool isJumping;
     bool isShooting;
     bool isTakingDamage;
     bool isInvincible;
-    bool isFacingRight; // default facing for static assets, must be initialized
-    bool hitSideRight; // the side where player is hit
+    bool isFacingRight;
+
+    bool hitSideRight;
 
     bool freezeInput;
     bool freezePlayer;
-    bool freezeBullets; // player related
+    bool freezeBullets;
+
     float shootTime;
     bool keyShootRelease;
 
@@ -31,15 +35,12 @@ public class PlayerController : MonoBehaviour
     public int currentHealth;
     public int maxHealth = 28;
 
-    [SerializeField] float moveSpeed = 3f;
-    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float moveSpeed = 1.5f;
+    [SerializeField] float jumpSpeed = 3.7f;
 
     [SerializeField] int bulletDamage = 1;
-
-    [SerializeField] float bulletSpeed = 5;
-
+    [SerializeField] float bulletSpeed = 5f;
     [SerializeField] Transform bulletShootPos;
-
     [SerializeField] GameObject bulletPrefab;
 
     [SerializeField] AudioClip jumpLandedClip;
@@ -52,49 +53,59 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        // get handles to components
         animator = GetComponent<Animator>();
         box2d = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
 
+        // sprite defaults to facing right
         isFacingRight = true;
 
+        // start at full health
         currentHealth = maxHealth;
+
+#if UNITY_STANDALONE
+        // disable screen input canvas if not android or ios
+        GameObject.Find("InputCanvas").SetActive(false);
+#endif
     }
 
-    private void FixedUpdate() {
-        isGrounded = false; // determine if touching the ground
+    private void FixedUpdate()
+    {
+        isGrounded = false;
         Color raycastColor;
         RaycastHit2D raycastHit;
         float raycastDistance = 0.05f;
         int layerMask = 1 << LayerMask.NameToLayer("Ground");
         // ground check
-        Vector3 box_origin = box2d.bounds.center; 
-        box_origin.y = box2d.bounds.min.y + (box2d.bounds.extents.y / 4f); 
+        Vector3 box_origin = box2d.bounds.center;
+        box_origin.y = box2d.bounds.min.y + (box2d.bounds.extents.y / 4f);
         Vector3 box_size = box2d.bounds.size;
-        box_size.y = box2d.bounds.size.y / 4f; // mini box unter the player
-        // check if touching down layer
+        box_size.y = box2d.bounds.size.y / 4f;
         raycastHit = Physics2D.BoxCast(box_origin, box_size, 0f, Vector2.down, raycastDistance, layerMask);
+        // player box colliding with ground layer
         if (raycastHit.collider != null)
         {
             isGrounded = true;
+            // just landed from jumping/falling
             if (isJumping)
             {
-                SoundManager.Instance.Play(jumpLandedClip);
                 isJumping = false;
+                SoundManager.Instance.Play(jumpLandedClip);
             }
         }
-        // raycast draw debug lines, visible in the Scene (green: touches; red: no touch)
+        // draw debug lines
         raycastColor = (isGrounded) ? Color.green : Color.red;
         Debug.DrawRay(box_origin + new Vector3(box2d.bounds.extents.x, 0), Vector2.down * (box2d.bounds.extents.y / 4f + raycastDistance), raycastColor);
         Debug.DrawRay(box_origin - new Vector3(box2d.bounds.extents.x, 0), Vector2.down * (box2d.bounds.extents.y / 4f + raycastDistance), raycastColor);
         Debug.DrawRay(box_origin - new Vector3(box2d.bounds.extents.x, box2d.bounds.extents.y / 4f + raycastDistance), Vector2.right * (box2d.bounds.extents.x * 2), raycastColor);
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // taking damage from projectiles, touching enemies, or other environment objects
         if (isTakingDamage)
         {
             animator.Play("Player_Hit");
@@ -104,9 +115,8 @@ public class PlayerController : MonoBehaviour
         PlayerDebugInput();
         PlayerDirectionInput();
         PlayerJumpInput();
-        PlayerShootinput();
+        PlayerShootInput();
         PlayerMovement();
-
     }
 
     void PlayerDebugInput()
@@ -126,11 +136,11 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Freeze Bullets: " + freezeBullets);
         }
 
-        // E for Explosions
+        // E for explosions
         if (Input.GetKeyDown(KeyCode.E))
         {
             Defeat();
-            Debug.Log("Defeat");
+            Debug.Log("Defeat()");
         }
 
         // I for Invincible
@@ -159,120 +169,43 @@ public class PlayerController : MonoBehaviour
     {
         if (!freezeInput)
         {
-        keyHorizontal = Input.GetAxisRaw("Horizontal");
+#if UNITY_STANDALONE
+            // get keyboard input
+            keyHorizontal = Input.GetAxisRaw("Horizontal");
+            keyVertical = Input.GetAxisRaw("Vertical");
+#endif
 
+#if UNITY_ANDROID || UNITY_IOS
+            // get on-screen virtual input
+            keyHorizontal = SimpleInput.GetAxisRaw("Horizontal");
+            keyVertical = SimpleInput.GetAxisRaw("Vertical");
+#endif
         }
     }
 
     void PlayerJumpInput()
     {
+#if UNITY_STANDALONE
         // get keyboard input
         if (!freezeInput)
         {
             keyJump = Input.GetKeyDown(KeyCode.Space);
         }
-
-        // rb2d.velocity = new Vector2(keyHorizontal * moveSpeed, rb2d.velocity.y);
+#endif
     }
 
-
-    void PlayerMovement()
-    {
-        if (keyHorizontal < 0) // pressing LEFT
-        {
-            if (isFacingRight)
-            {
-                Flip();
-            }
-            if (isGrounded) 
-            {
-                if (isShooting)
-                {
-                    animator.Play("Player_RunShoot");
-                }
-                else 
-                {
-                    animator.Play("Player_Run");
-                }
-            }
-            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
-
-        }
-        else if (keyHorizontal > 0) // pressing RIGHT
-        {
-            if (!isFacingRight)
-            {
-                Flip();
-            }
-            if (isGrounded) 
-            {
-                if (isShooting)
-                {
-                    animator.Play("Player_RunShoot");
-                }
-                else 
-                {
-                    animator.Play("Player_Run");
-                }
-            }
-            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-        }
-        else // nothing pressed
-        {
-            if (isGrounded) 
-            {
-                if (isShooting)
-                {
-                    animator.Play("Player_Shoot");
-                }
-                else 
-                {
-                    animator.Play("Player_Idle");
-                }
-            }
-            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
-
-        }
-
-        if (keyJump && isGrounded) // only jump if isGrounded
-        {
-            if (isShooting)
-            {
-                animator.Play("Player_Shoot");
-            }
-            else 
-            {
-                animator.Play("Player_Jump");
-            }
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
-        }
-
-        if (!isGrounded) // falling
-        {
-            isJumping = true;
-            if (isShooting)
-            {
-                animator.Play("Player_JumpShoot");
-            }
-            else 
-            {
-                animator.Play("Player_Jump");
-            }
-
-        }
-
-    }
-
-    void PlayerShootinput()
+    void PlayerShootInput()
     {
         float shootTimeLength = 0;
         float keyShootReleaseTimeLength = 0;
 
+#if UNITY_STANDALONE
         // get keyboard input
         if (!freezeInput)
         {
             keyShoot = Input.GetKey(KeyCode.C);
         }
+#endif
 
         // shoot key is being pressed and key release flag true
         if (keyShoot && keyShootRelease)
@@ -300,28 +233,131 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void PlayerMovement()
+    {
+        // left arrow key - moving left
+        if (keyHorizontal < 0)
+        {
+            // facing right while moving left - flip
+            if (isFacingRight)
+            {
+                Flip();
+            }
+            // grounded play run animation
+            if (isGrounded)
+            {
+                // play run shoot or run animation
+                if (isShooting)
+                {
+                    animator.Play("Player_RunShoot");
+                }
+                else
+                {
+                    animator.Play("Player_Run");
+                }
+            }
+            // negative move speed to go left
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+        }
+        else if (keyHorizontal > 0) // right arrow key - moving right
+        {
+            // facing left while moving right - flip
+            if (!isFacingRight)
+            {
+                Flip();
+            }
+            // grounded play run animation
+            if (isGrounded)
+            {
+                // play run shoot or run animation
+                if (isShooting)
+                {
+                    animator.Play("Player_RunShoot");
+                }
+                else
+                {
+                    animator.Play("Player_Run");
+                }
+            }
+            // positive move speed to go right
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+        }
+        else   // no movement
+        {
+            // grounded play idle animation
+            if (isGrounded)
+            {
+                // play shoot or idle animation
+                if (isShooting)
+                {
+                    animator.Play("Player_Shoot");
+                }
+                else
+                {
+                    animator.Play("Player_Idle");
+                }
+            }
+            // no movement zero x velocity
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
+        }
+
+        // pressing jump while grounded - can only jump once
+        if (keyJump && isGrounded)
+        {
+            // play jump/jump shoot animation and jump speed on y velocity
+            if (isShooting)
+            {
+                animator.Play("Player_JumpShoot");
+            }
+            else
+            {
+                animator.Play("Player_Jump");
+            }
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+        }
+
+        // while not grounded play jump animation (jumping or falling)
+        if (!isGrounded)
+        {
+            // triggers jump landing sound effect in FixedUpdate
+            isJumping = true;
+            // jump or jump shoot animation
+            if (isShooting)
+            {
+                animator.Play("Player_JumpShoot");
+            }
+            else
+            {
+                animator.Play("Player_Jump");
+            }
+        }
+    }
 
     void Flip()
     {
+        // invert facing direction and rotate object 180 degrees on y axis
         isFacingRight = !isFacingRight;
         transform.Rotate(0f, 180f, 0f);
     }
 
     void ShootBullet()
     {
+        // create bullet from prefab gameobject
         GameObject bullet = Instantiate(bulletPrefab, bulletShootPos.position, Quaternion.identity);
+        // set its name to that of the prefab so it doesn't include "(Clone)" when instantiated
+        bullet.name = bulletPrefab.name;
+        // set bullet damage amount, speed, direction bullet will travel along the x, and fire!
         bullet.GetComponent<BulletScript>().SetDamageValue(bulletDamage);
         bullet.GetComponent<BulletScript>().SetBulletSpeed(bulletSpeed);
         bullet.GetComponent<BulletScript>().SetBulletDirection((isFacingRight) ? Vector2.right : Vector2.left);
         bullet.GetComponent<BulletScript>().SetDestroyDelay(5f);
         bullet.GetComponent<BulletScript>().Shoot();
-
         SoundManager.Instance.Play(shootBulletClip);
     }
-
     public void HitSide(bool rightSide)
     {
-        hitSideRight = rightSide; 
+        // determines the push direction of the hit animation
+        hitSideRight = rightSide;
     }
 
     public void Invincible(bool invincibility)
@@ -331,11 +367,14 @@ public class PlayerController : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        // take damage if not invincible
         if (!isInvincible)
         {
+            // take damage amount from health and update the health bar
             currentHealth -= damage;
             currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
             UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+            // no more health means defeat, otherwise take damage
             if (currentHealth <= 0)
             {
                 Defeat();
@@ -349,6 +388,9 @@ public class PlayerController : MonoBehaviour
 
     void StartDamageAnimation()
     {
+        // once isTakingDamage is true in the Update function we'll play the Hit animation
+        // here we go invincible so we don't repeatedly take damage, determine the X push force
+        // depending which side we were hit on, and then apply that force
         if (!isTakingDamage)
         {
             isTakingDamage = true;
@@ -359,9 +401,7 @@ public class PlayerController : MonoBehaviour
             if (hitSideRight) hitForceX = -hitForceX;
             rb2d.velocity = Vector2.zero;
             rb2d.AddForce(new Vector2(hitForceX, hitForceY), ForceMode2D.Impulse);
-
             SoundManager.Instance.Play(takingDamageClip);
-
         }
     }
 
@@ -374,17 +414,16 @@ public class PlayerController : MonoBehaviour
         Invincible(false);
         FreezeInput(false);
         animator.Play("Player_Hit", -1, 0f);
-
     }
 
     void StartDefeatAnimation()
     {
+        // freeze player and input and go KABOOM!
         FreezeInput(true);
         FreezePlayer(true);
         GameObject explodeEffect = Instantiate(explodeEffectPrefab);
         explodeEffect.name = explodeEffectPrefab.name;
         explodeEffect.transform.position = sprite.bounds.center;
-
         SoundManager.Instance.Play(explodeEffectClip);
         Destroy(gameObject);
     }
@@ -397,34 +436,70 @@ public class PlayerController : MonoBehaviour
 
     void Defeat()
     {
+        // tell the game manager we died so it can take control
         GameManager.Instance.PlayerDefeated();
+        // we died! player defeat animation - half second delay
         Invoke("StartDefeatAnimation", 0.5f);
     }
 
     public void FreezeInput(bool freeze)
     {
+        // freeze/unfreeze user input
         freezeInput = freeze;
-
     }
 
     public void FreezePlayer(bool freeze)
     {
-         if (freeze)
+        // freeze/unfreeze the player on screen
+        // zero animation speed and freeze XYZ rigidbody constraints
+        if (freeze)
         {
             freezePlayer = true;
-            animator.speed = 0;
             rb2dConstraints = rb2d.constraints;
+            animator.speed = 0;
             rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
-            rb2d.velocity = Vector2.zero;
         }
         else
         {
             freezePlayer = false;
             animator.speed = 1;
             rb2d.constraints = rb2dConstraints;
-            // ?? rb2d.velocity = bulletDirection * bulletSpeed;
-        }         
+        }
     }
 
+    public void MobileShootWrapper()
+    {
+        // wrapper function for button handler script
+        // can't directly call coroutines
+        if (!freezeInput)
+        {
+            StartCoroutine(MobileShoot());
+        }
+    }
 
+    private IEnumerator MobileShoot()
+    {
+        // press shoot and release
+        keyShoot = true;
+        yield return new WaitForSeconds(0.01f); // delay between frames with a coroutine
+        keyShoot = false;
+    }
+
+    public void MobileJumpWrapper()
+    {
+        // wrapper function for button handler script
+        // can't directly call coroutines
+        if (!freezeInput)
+        {
+            StartCoroutine(MobileJump());
+        }
+    }
+
+    private IEnumerator MobileJump()
+    {
+        // press jump and release
+        keyJump = true;
+        yield return new WaitForSeconds(0.01f); // delay between frames with a coroutine
+        keyJump = false;
+    }
 }
